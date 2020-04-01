4f00c6556f368ae4a156be2f53d351b82275d080
TOP_Green <metallgreen@gmail.com>
2020-04-01 18:34:14 +0300

    readme

--- filmoteka/src/app/components/main/main.component.ts
+++ filmoteka/src/app/components/main/main.component.ts
@@ -15,16 +15,11 @@ export class MainComponent implements OnInit {
   filmTitle;
 
   constructor(private firebase: FirebaseService) {
-    // this.films = this.firebase.getData("films")
   }
 
 
   async ngOnInit() {
     this.films = await this.firebase.getData("films")
-    console.log(this.films)
-    // console.log(data)
-    // this.films = data.map(el => el.payload.doc.data());
-    // console.log(this.films);
   }
 
   public async addFilm() {
